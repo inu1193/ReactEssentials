93ee78c633803b6704432b20c2bc16259b3dfc8c
unknown <inayath2410@gmail.com>
2024-08-13 06:47:45 +0530

    Add Dynamic function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import { CORE_CONCEPTS } from './data.js'
-import CoreConcept from './components/CoreConcept';
+import CoreConcept from './components/CoreConcept/CoreConcept';
+import { TabButton } from './components/TabButton/TabButton';
 
 function App() {
+  function handleSelect(selectedButton){
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -20,6 +24,16 @@ function App() {
       <CoreConcept {...CORE_CONCEPTS[3]}/>
       </ul>
       </section>
+      <section id='examples'>
+        <h2>Example</h2>
+        <menu>
+        <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
+        <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+        <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
+        <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
+        </menu>
+      </section>
+      Dynamic Content
       </main>
     </div>
   );
---
+++ src/components/TabButton/TabButton.jsx
@@ -0,0 +1,3 @@
+export function TabButton({children , onSelect}){
+    return(<li><button onClick = {onSelect}>{children}</button></li>)
+}
